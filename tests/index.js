0b3d29bba96a10f13fbd3a237f47a6f33b531b5f
Blaine Bublitz <blaine.bublitz@gmail.com>
2017-05-06 12:10:56 -0500

    Add struct tests

--- tests/index.js
+++ tests/index.js
@@ -6,35 +6,6 @@ let thriftParser = require('../');
 
 describe('thriftParser', function() {
 
-  describe('struct', function() {
-
-    it('parses simple struct', function(done) {
-      let content = `
-        struct MyStruct {
-          1: required int id,
-        }
-      `;
-
-      let expected = {
-        struct: {
-          MyStruct: [
-            {
-              id: 1,
-              option: 'required',
-              type: 'int',
-              name: 'id'
-            }
-          ]
-        }
-      };
-
-      let ast = thriftParser(content);
-
-      expect(ast).toEqual(expected);
-      done();
-    });
-  });
-
   describe('regressions', function() {
 
     it('throws on number with `-` in the middle', function(done) {
